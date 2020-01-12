fafae52fd8195d7fba2f333642fadc41c1d2f8d6
Pallavi Shah <emailpallavishah@gmail.com>
2020-01-12 16:50:26 -0600

    Studio Area program.cs modified

--- Class1/Studio/Area/Area/Program.cs
+++ Class1/Studio/Area/Area/Program.cs
@@ -6,17 +6,40 @@ namespace Area
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a radius: ");
-            double radius = Convert.ToDouble(Console.ReadLine());
-
-            while (radius <= 0)
+            double radius = 0;
+            do
             {
-                Console.WriteLine("\nError: Radius must be Greater Than Zero: ");
-                Console.Write("\nEnter radius of the circle: ");
-                radius = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter a radius: ");
+                string strRadius = Console.ReadLine();
+                if (strRadius == "")
+                {
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        radius = Double.Parse(strRadius);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error: Radius must be a number.");
+                        break;
+                    }
+                }
+                if (radius<0)
+                {
+                    Console.WriteLine("\nError: Radius must be Greater Than Zero: ");
+                    break;
+                }
+                else
+                {
+                    double Area = 3.14 * Math.Pow(radius, 2);
+                    Console.WriteLine($"\n The area of a circle with radius {radius} is : " + Area);
+                }
             }
-            double Area = 3.14 * Math.Pow(radius, 2);
-            Console.WriteLine($"\n The area of a circle with radius {radius} is : " + Area);
+            while (radius<0);
+            Console.ReadLine();
         }
     }
 }
