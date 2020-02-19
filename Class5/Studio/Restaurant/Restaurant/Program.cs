28df29fd8cd6c7e79c5a71bf357622237a509e01
Pallavi Shah <emailpallavishah@gmail.com>
2020-02-19 09:58:15 -0600

    Restaurant Studio Class 5

---
+++ Class5/Studio/Restaurant/Restaurant/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+
+     public class Menu
+     {
+        public static List<MenuItem> MenuItems = new List<MenuItem>();
+        private static DateTime lastUpdated;
+     }
+      public class MenuItem
+    {
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public enum category { appetizer, maincourse, dessert };
+        private DateTime dateadded;
+        public bool IsNew;
+
+        public MenuItem(string aItemName, double aPrice, string aDescription)
+        {
+            ItemName = aItemName;
+            Price = aPrice;
+            Description = aDescription;
+        }
+    }
+}
